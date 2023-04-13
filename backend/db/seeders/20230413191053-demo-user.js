4176d294a444a3a4e7d182c38f2deb845bd0b98e
Taylor McClerin <mcclerinette@gmail.com>
2023-04-13 12:29:42 -0700

    Added users table migration and seeder files

---
+++ backend/db/seeders/20230413191053-demo-user.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const bcrypt = require("bcryptjs")
+
+let options = {}
+if(process.env.NODE_ENV === 'production'){
+  options.schema = process.env.SCHEMA
+}
+
+
+
+const demoUser = [
+  {
+    username: 'testUserName',
+    email: 'test@gmail.com',
+    hashedPassword: 'testPassword'
+}
+]
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize)=> {
+   options.tableName = 'Users';
+   return queryInterface.bulkInsert(options,[
+    {
+      username: 'testUserName',
+      email: 'test@gmail.com',
+      hashedPassword: bcrypt.hashSync('testPassword')
+    },
+    {
+      email: 'demo@user.io',
+      username: 'Demo-licious',
+      hashedPassword: bcrypt.hashSync('password2')
+    },
+    {
+      email: 'user1@user.io',
+      username: 'FakeUser1',
+      hashedPassword: bcrypt.hashSync('password2')
+    },
+  ], {});
+},
+
+  down: async(queryInterface, Sequelize)=> {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options,{
+      username: { [Op.in]: ['testUserName', 'Demo-licious', 'FakeUser1']}
+    })
+  }
+};
